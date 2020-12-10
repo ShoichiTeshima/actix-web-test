f55a3e02d066d8efe9827d60620d34727d69bb79
steshima <steshima@sonix.asia>
2020-12-10 16:41:43 +0900

    change bind address

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn health_check() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().configure(routes::routes))
-        .bind("0.0.0.0:8080")?
+        .bind("localhost:8080")?
         .run()
         .await
 }
