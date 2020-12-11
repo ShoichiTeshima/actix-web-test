9572b7df5bdeaa838c4961782963499e5f180eef
steshima <steshima@sonix.asia>
2020-12-11 12:04:23 +0900

    db local test

--- src/db_client.rs
+++ src/db_client.rs
@@ -5,7 +5,9 @@ use std::env;
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
-    let url = env::var("DATABASE_URL").unwrap_or_else(|_| panic!("DATABASE_URL must be set"));
+    // 環境ごとに切り替える
+    let url = "db";
+    // let url = env::var("DATABASE_URL").unwrap_or_else(|_| panic!("DATABASE_URL must be set"));
     MysqlConnection::establish(&url)
         .unwrap_or_else(|_| panic!(format!("Error connecting to {}", url)))
 }
